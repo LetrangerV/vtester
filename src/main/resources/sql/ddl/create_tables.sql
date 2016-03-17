d30ce81399b1a9629f26408e4abbbe1bf0fc4856
LetrangerV <strokanviktor@gmail.com>
2016-03-17 15:23:37 +0600

    Change db schema initialization order

--- src/main/resources/sql/ddl/create_tables.sql
+++ src/main/resources/sql/ddl/create_tables.sql
@@ -3,6 +3,11 @@ create table quizzes(
   title varchar(100)
 );
 
+create table question_types(
+  question_type_id int primary key auto_increment,
+  type varchar(20)
+);
+
 create table questions(
   question_id int primary key auto_increment,
   text varchar(350),
@@ -10,11 +15,6 @@ create table questions(
   type_id int,
   constraint 'fk_quiz' foreign key (quiz_id) references quizzes(quiz_id) on update cascade on delete cascade,
   constraint 'fk_type' foreign key (type_id) references question_types(question_type_id) on update cascade on delete cascade
-);
-
-create table question_types(
-  question_type_id int primary key auto_increment,
-  type varchar(20)
 );
 
 insert into question_types (type) values ('one_answer');
