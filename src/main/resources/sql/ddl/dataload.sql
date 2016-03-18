d03596b7df9f7c4496353e60ce9459aab7a3a4ef
LetrangerV <strokanviktor@gmail.com>
2016-03-18 11:50:21 +0600

    Add new tables to database schema to match domain model

---
+++ src/main/resources/sql/ddl/dataload.sql
@@ -0,0 +1,4 @@
+insert into question_types (type) values ('one_answer');
+insert into question_types (type) values ('multi_answer');
+insert into question_types (type) values ('mapping');
+insert into question_types (type) values ('user_input');
---
+++ src/main/resources/sql/ddl/schema.sql
@@ -0,0 +1,60 @@
+create table quizzes(
+  quiz_id int primary key auto_increment,
+  title varchar(100)
+);
+
+create table question_types(
+  question_type_id int primary key auto_increment,
+  type varchar(20)
+);
+
+create table questions(
+  question_id int primary key auto_increment,
+  text varchar(350),
+  quiz_id int,
+  type_id int,
+  constraint 'fk__questions__quizzes' foreign key (quiz_id) references quizzes(quiz_id) on update cascade on delete cascade,
+  constraint 'fk__questions__question_types' foreign key (type_id) references question_types(question_type_id) on update cascade on delete cascade
+);
+
+create table options(
+  option_id int primary key auto_increment,
+  text varchar(100),
+  is_right boolean,
+  question_id int,
+  constraint 'fk__options__questions' foreign key (question_id) references questions(question_id) on update cascade on delete cascade
+);
+
+create table classes(
+  class_id int primary key auto_increment,
+  name varchar(3)
+);
+
+create table students(
+  student_id int primary key auto_increment,
+  first_name varchar(35),
+  last_name varchar(35),
+  email varchar(255),
+  class_id int,
+  constraint 'fk__students__classes' foreign key (class_id) references classes(class_id) on update cascade on delete cascade
+);
+
+create table assigned_quizzes(
+  assigned_quiz_id int primary key auto_increment,
+  quiz_id int,
+  constraint 'fk__assigned_quizzes__quizzes' foreign key (quiz_id) references quizzes(quiz_id) on update cascade on delete cascade
+);
+
+create table passed_quizzes(
+  passed_quiz_id int primary key auto_increment,
+  quiz_id int,
+  constraint 'fk__passed_quizzes__quizzes' foreign key (quiz_id) references quizzes(quiz_id) on update cascade on delete cascade
+);
+
+create table marks(
+  mark_id int primary key auto_increment,
+  mark int,
+  student_id int,
+  passed_quiz_id int,
+  constraint 'fk__marks__passed_quizzes' foreign key (passed_quiz_id) references quizzes(quiz_id) on update cascade on delete cascade
+);
